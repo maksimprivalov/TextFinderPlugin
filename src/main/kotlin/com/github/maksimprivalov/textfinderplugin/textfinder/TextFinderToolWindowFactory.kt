815f9a9a8afdef58ee35e3bd6f0adda17fb2b555
maksimprivalov <mprivalovs@gmail.com>
2025-10-19 22:56:07 +0200

    add: ToolWindowFactory implementation & configuration

---
+++ src/main/kotlin/com/github/maksimprivalov/textfinderplugin/textfinder/TextFinderToolWindowFactory.kt
@@ -0,0 +1,14 @@
+package com.github.maksimprivalov.textfinderplugin.textfinder
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
+
+class TextFinderToolWindowFactory : ToolWindowFactory {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val panel = TextFinderPanel()
+        val content = ContentFactory.getInstance().createContent(panel, "", false)
+        toolWindow.contentManager.addContent(content)
+    }
+}
